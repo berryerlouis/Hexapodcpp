0251fe2f9af875a79d2c31575f656b3df7c2d16b
Louis Berryer <louis.berryer@non.se.com>
2024-07-18 23:06:02 +0200

    connvert param from message

--- HMI/js/protocol/Message.js
+++ HMI/js/protocol/Message.js
@@ -8,8 +8,11 @@ export class Message {
     size;
     params;
     raw;
+    index;
 
-    constructor() { }
+    constructor() {
+        this.index = 0;
+    }
 
     build(cluster = undefined, command = undefined, size = 0, params = null) {
 
@@ -17,6 +20,7 @@ export class Message {
         this.command = Clusters.findCommandByName(command);
         this.size = size;
         this.params = params;
+        this.index = 0;
         this.raw = Protocol.encode(this.cluster, this.command, this.size, this.params);
         return this;
     }
@@ -28,5 +32,85 @@ export class Message {
             '\t' + this.raw +
             '\t' + this.cluster.name +
             '\t' + this.command.name;
+    }
+
+    fetchInt8U() {
+        let num = Infinity
+        if (this.index + 1 <= this.size) {
+            num = parseInt(this.params[this.index], 16);
+            this.index++;
+        }
+        return num;
+    }
+
+    fetchInt8S() {
+        let num = this.fetchInt8U();
+        if (num & 0x80) { num = -(0x100 - num); }
+        return num;
+    }
+
+    fetchInt16U() {
+        let num = Infinity
+        if (this.index + 2 <= this.size) {
+            num = parseInt(
+                this.params[this.index + 1] +
+                this.params[this.index], 16);
+            this.index += 2;
+        }
+        return num;
+    }
+
+    fetchInt16S() {
+        let num = this.fetchInt16U();
+        if (num & 0x8000) { num = -(0x10000 - num); }
+        return num;
+    }
+
+    fetchInt24U() {
+        let num = Infinity
+        if (this.index + 3 <= this.size) {
+            num = parseInt(
+                this.params[this.index + 2] +
+                this.params[this.index + 1] +
+                this.params[this.index], 16);
+            this.index += 3;
+        }
+        return num;
+    }
+
+    fetchInt32U() {
+        let num = Infinity
+        if (this.index + 4 <= this.size) {
+            num = parseInt(
+                this.params[this.index + 3] +
+                this.params[this.index + 2] +
+                this.params[this.index + 1] +
+                this.params[this.index], 16);
+            this.index += 4;
+        }
+        return num;
+    }
+
+    fetchInt32S() {
+        let num = this.fetchInt32U();
+        if (num & 0x800000) { num = -(0x1000000 - num); }
+        return num;
+    }
+
+    fetchInt64U() {
+        let num = Infinity
+        if (this.index + 8 <= this.size) {
+            num = parseInt(
+                this.params[this.index + 7] +
+                this.params[this.index + 6] +
+                this.params[this.index + 5] +
+                this.params[this.index + 4] +
+                this.params[this.index + 3] +
+                this.params[this.index + 2] +
+                this.params[this.index + 1] +
+                this.params[this.index], 16);
+            this.index += 8;
+        }
+        return num;
     }
 };
