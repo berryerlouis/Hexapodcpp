b8e0df1861fb3156fd762e7f6ebbdbe7fb06d6e4
Louis BERRYER <louisberryer@gmail.com>
2024-08-26 22:24:51 +0200

    Add unit test cmuunication to debug signed int in frame

--- unittests/src/cmp/communication/UT_CMP_COMMUNICATION.cpp
+++ unittests/src/cmp/communication/UT_CMP_COMMUNICATION.cpp
@@ -1,13 +1,13 @@
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
-
+#include "../../../mock/bot/MockBody.h"
 #include "../../../mock/cmp/MockLed.h"
 #include "../../../mock/drv/MockUart.h"
 #include "../../../mock/clu/MockClusters.h"
 
-
 #include "../../../../src/Cluster/Decoding/Protocol.h"
+#include "../../../../src/Cluster/Body/ClusterBody.h"
 #include "../../../../src/Component/Communication/Communication.h"
 
 using ::testing::_;
@@ -20,9 +20,11 @@ namespace Communication {
 class UT_CMP_COMMUNICATION : public ::testing::Test {
 protected:
 	UT_CMP_COMMUNICATION() :
+		mMockBody(),
 		mMockUart(),
 		mMockClusters(),
 		mMockLed(),
+		mClusterBody( mMockBody ),
 		mCommunication( mMockUart, mMockClusters, mMockLed )
 	{
 	}
@@ -38,9 +40,11 @@ protected:
 	virtual ~UT_CMP_COMMUNICATION() = default;
 
 	/* Mocks */
+	StrictMock <Bot::Body::MockBody> mMockBody;
 	StrictMock <Driver::Uart::MockUart> mMockUart;
 	StrictMock <Cluster::Clusters::MockClusters> mMockClusters;
 	StrictMock <Component::Led::MockLed> mMockLed;
+	Cluster::Body::ClusterBody mClusterBody;
 
 	/* Test class */
 	Communication mCommunication;
@@ -131,7 +135,31 @@ TEST_F( UT_CMP_COMMUNICATION, Update_Ko_1frame )
 	EXPECT_CALL( mMockLed, Off() ).WillOnce( Return( Core::CoreStatus::CORE_OK ) );
 	EXPECT_CALL( mMockUart, Send( Matcher <const uint8_t *>( _ ), _ ) ).Times( 1U );
 
+	for ( size_t i = 0; i < strlen( bufferRx ); i++ )
+	{
+		EXPECT_CALL( mMockUart, DataAvailable() ).WillOnce( Return( 8U ) );
+		EXPECT_CALL( mMockUart, Read() ).WillOnce( Return( bufferRx[i] ) );
+		mCommunication.Update( 0UL );
+	}
 
+	EXPECT_TRUE( success );
+}
+
+TEST_F( UT_CMP_COMMUNICATION, Update_Ok_BodySet )
+{
+	Core::CoreStatus success  = Core::CoreStatus::CORE_ERROR;
+	const char *     bufferRx = "<05000e178000000000000000000000f401>";
+
+	EXPECT_CALL( mMockLed, Initialize() ).WillOnce( Return( Core::CoreStatus::CORE_OK ) );
+
+	success = mCommunication.Initialize();
+
+	EXPECT_CALL( mMockLed, On() ).WillOnce( Return( Core::CoreStatus::CORE_OK ) );
+	EXPECT_CALL( mMockLed, Off() ).WillOnce( Return( Core::CoreStatus::CORE_OK ) );
+	EXPECT_CALL( mMockUart, Send( Matcher <const uint8_t *>( _ ), _ ) ).Times( 1U );
+	EXPECT_CALL( mMockClusters, GetCluster( Cluster::EClusters::BODY ) ).WillOnce( Return( &mClusterBody ) );
+	EXPECT_CALL( mMockBody, SetPositionRotation( _, _, _ ) ).Times( 1U );
+	
 	for ( size_t i = 0; i < strlen( bufferRx ); i++ )
 	{
 		EXPECT_CALL( mMockUart, DataAvailable() ).WillOnce( Return( 8U ) );
