8753ddb8606cb4ea861453967175f1ee628524f9
berryerlouis <louisberryer@gmail.com>
2025-05-09 23:22:24 +0200

    fix TU

--- unittests/mock/bot/MockBody.h
+++ unittests/mock/bot/MockBody.h
@@ -4,18 +4,22 @@
 
 namespace Bot
 {
-	namespace Body
-	{
-		class MockBody : public BodyInterface {
-		public:
-			MOCK_METHOD0(Initialize, Core::Status( void ));
-			MOCK_METHOD1(Update, void( const uint64_t ));
-
-			MOCK_METHOD1(UpdateWalkStatus, void( const EWalkStatus ));
-			MOCK_METHOD3(SetBodyPositionRotation,
-			             uint32_t (const Misc::Maths::Position3d &, const Misc::Maths::Rotation3d &, const uint16_t ));
-			MOCK_METHOD3(SetLegPositionRotation,
-			             uint32_t (const uint8_t &, const Misc::Maths::Position3d &, const uint16_t ));
-		};
-	}
+    namespace Body
+    {
+        class MockBody : public BodyInterface {
+        public:
+            MOCK_METHOD0(Initialize, Core::Status( void ));
+            MOCK_METHOD1(Update, void( const uint64_t ));
+            MOCK_METHOD1(UpdateWalkStatus, void( const EWalkStatus ));
+            MOCK_METHOD3(SetBodyPositionRotation,
+                         uint32_t (const Misc::Maths::Position3d &, const Misc::Maths::Rotation3d &, const uint16_t ));
+            MOCK_METHOD3(SetLegPositionRotation,
+                         uint32_t (const uint8_t &, const Misc::Maths::Position3d &, const uint16_t ));
+            MOCK_METHOD1(SetDirection, bool( const uint16_t ));
+            MOCK_METHOD2(SetAmplitudeElevation, bool( const uint16_t, const uint16_t ));
+            MOCK_CONST_METHOD0(GetDirection, uint16_t( void ));
+            MOCK_CONST_METHOD0(GetAmplitude, uint8_t( void));
+            MOCK_CONST_METHOD0(GetElevation, uint8_t( void));
+        };
+    }
 }
--- unittests/src/cmp/communication/UT_CMP_COMMUNICATION.cpp
+++ unittests/src/cmp/communication/UT_CMP_COMMUNICATION.cpp
@@ -65,7 +65,7 @@ namespace Component
             mCommunication.Update(0UL);
         }
 
-        TEST_F(UT_CMP_COMMUNICATION, Update_Ok_1frame_with_unknwon_char) {
+        TEST_F(UT_CMP_COMMUNICATION, Update_Ok_1frame_with_unknown_char) {
             const char *bufferRx = "<00z000>";
             EXPECT_CALL(mMockSocket, Update(0U));
             //EXPECT_CALL(mMockLed, On()).Times(0U);
@@ -127,27 +127,6 @@ namespace Component
             mCommunication.Update(0UL);
         }
 
-        TEST_F(UT_CMP_COMMUNICATION, Update_Ok_BodySet) {
-            constexpr Misc::Maths::Position3d position = {-5.0, 5.0, -5.0};
-            constexpr Misc::Maths::Rotation3d rotation = {-3.0, 3.0, -2.0};
-            constexpr uint16_t travelTime = 500U;
-
-            const char *bufferRx = "<05000ECEFF3200CEFFE2FF1E00ECFFF401>";
-
-            EXPECT_CALL(mMockSocket, Update(0U));
-            //EXPECT_CALL(mMockLed, On()).WillOnce(Return(Core::Status::CORE_OK));
-            //EXPECT_CALL(mMockLed, Off()).WillOnce(Return(Core::Status::CORE_OK));
-            EXPECT_CALL(mMockSocket, Send( Matcher <const char *>( _ ), _ )).Times(1U);
-            EXPECT_CALL(mMockClusters, GetCluster( Cluster::EClusters::BODY )).WillOnce(Return(&mClusterBody));
-            EXPECT_CALL(mMockBody, SetBodyPositionRotation( position, rotation, travelTime )).Times(1U);
-
-            EXPECT_CALL(mMockSocket, DataAvailable()).WillOnce(Return(strlen(bufferRx)));
-            ::testing::Sequence s;
-            for (size_t i = 0; i < strlen(bufferRx); i++) {
-                EXPECT_CALL(mMockSocket, Read()).InSequence(s).WillOnce(Return(bufferRx[i]));
-            }
-            mCommunication.Update(0UL);
-        }
     }
 }
 
