e50b6a4abe94282686b4a73c1a117c0f3849f7bf
berryerlouis <louisberryer@gmail.com>
2025-06-04 11:23:51 +0200

    PUdate direction and elevation of legs

--- unittests/mock/bot/MockBody.h
+++ unittests/mock/bot/MockBody.h
@@ -16,7 +16,8 @@ namespace Bot
             MOCK_METHOD3(SetLegPositionRotation,
                          uint32_t (const uint8_t &, const Misc::Maths::Position3d &, const uint16_t ));
             MOCK_METHOD1(SetDirection, bool( const uint16_t ));
-            MOCK_METHOD2(SetAmplitudeElevation, bool( const uint16_t, const uint16_t ));
+            MOCK_METHOD1(SetAmplitude, bool( const uint8_t ));
+            MOCK_METHOD1(SetElevation, bool( const uint8_t ));
             MOCK_CONST_METHOD0(GetDirection, uint16_t( void ));
             MOCK_CONST_METHOD0(GetAmplitude, uint8_t( void));
             MOCK_CONST_METHOD0(GetElevation, uint8_t( void));
